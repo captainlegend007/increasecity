0bc3825d09f924eb2a5d1aff5a9617407fdc5ce1
captainlegend007 <emmaekagha6@gmail.com>
2025-08-26 17:26:31 +0100

    Updates

--- sw.js
+++ sw.js
@@ -78,35 +78,35 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "assets/Giving-B2Ss8--c.js",
+    "url": "assets/Giving-BWVGEkw3.js",
     "revision": null
   }, {
-    "url": "assets/index-B6y2lJTv.js",
+    "url": "assets/index-DjS9v27g.js",
     "revision": null
   }, {
     "url": "assets/index-DMDIcsSA.css",
     "revision": null
   }, {
-    "url": "assets/Resources-C4XBl66Y.js",
-    "revision": null
-  }, {
     "url": "assets/Resources-DDw0sW7T.css",
     "revision": null
   }, {
-    "url": "assets/Testimonies-BfN1vVsT.js",
+    "url": "assets/Resources-Dhkz8AbS.js",
     "revision": null
   }, {
     "url": "assets/Testimonies-DS9PXo0n.css",
     "revision": null
   }, {
-    "url": "assets/UpcomingPrograms-BtbBht9W.js",
+    "url": "assets/Testimonies-DvQDV9Kn.js",
     "revision": null
   }, {
     "url": "assets/UpcomingPrograms-CLJ-0I8R.css",
     "revision": null
   }, {
+    "url": "assets/UpcomingPrograms-D1Wqoy0h.js",
+    "revision": null
+  }, {
     "url": "index.html",
-    "revision": "7ce794af17555c71b519e9092d7372bf"
+    "revision": "767ceb5643262303004be2829e72cb10"
   }, {
     "url": "registerSW.js",
     "revision": "fde54991466ae94f524d8f984944e8b7"
